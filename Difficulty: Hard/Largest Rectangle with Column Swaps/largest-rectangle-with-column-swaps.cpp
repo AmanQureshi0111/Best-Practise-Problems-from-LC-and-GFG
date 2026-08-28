a5be258022c8b0bfbab334ff24449d5d890eb7d4
Aman Qureshi <amanq7362@gmail.com>
2026-08-28 22:23:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Largest Rectangle with Column Swaps/largest-rectangle-with-column-swaps.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int maxArea(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>> height(n,vector<int>(m,0));
+        for(int j=0;j<m;j++){
+            height[0][j]=mat[0][j];
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]) height[i][j]=1+height[i-1][j];
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            vector<int> row=height[i];
+            sort(row.rbegin(),row.rend());
+            for(int j=0;j<m;j++){
+                ans=max(ans,row[j]*(j+1));
+            }
+        }
+        return ans;
+    }
+};
